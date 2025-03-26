7107b738d12fd9655eeef2ec23df876be91f86d9
ONEONUORA <monweebube44@gmail.com>
2025-03-26 10:19:29 +0100

    Dapp

---
+++ src/app/dashboard/exam-page/page.tsx
@@ -0,0 +1,9 @@
+
+function Page(){
+    return(
+        <h1>Implement exam page here , create a component in the same folder is need be</h1>
+    )
+}
+
+
+export default Page;
---
+++ src/app/dashboard/institution/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+function Page(){
+    return(
+        <h1>Implement institution here , create a component in the same folder is need be</h1>
+    )
+}
+
+
+export default Page;
