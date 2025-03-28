c81c6de67614e9a4933337de60868e536f998c7f
Francis Longsaar <longsaarmuknaan@gmail.com>
2025-03-28 14:41:26 +0100

    Feat/registered exam page and modal #3 (#27)
    * Implementing registered exams page
    
    * Implementing registered exams page
    
    * adding style config for scroll bar
    
    * Initial commit
    
    * Adding page components

--- src/app/dashboard/exam-page/registered/page.tsx
+++ src/app/dashboard/exam-page/registered/page.tsx
@@ -1,20 +1,19 @@
 import DynamicHeader from "@/components/dashboard/exam/components/dynamic-header";
+import ExamsRegisteredComponent from "@/components/exams-registered/ExamsRegistered";
+import Image from "next/image";
 import { Suspense } from "react";
+import RobotChatImage from "../../../../../public/chat-bot.svg"
 
 export default function RegisteredPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div className="flex flex-col">
         <DynamicHeader />
-        <div className="container flex flex-1 items-center justify-center py-12">
-          <div className="text-center">
-            <h2 className="text-2xl font-bold">Registered Exams</h2>
-            <p className="mt-2 text-muted-foreground">
-              This page is under construction. Check back soon for your
-              registered exams.
-            </p>
-          </div>
+        <div className="self-end pt-14 text-[#ABABAB] flex gap-2">
+          <p>Chatbox</p>
+          <Image alt="robot-chat-image" width={20} height={20} src={RobotChatImage} />
         </div>
+        <ExamsRegisteredComponent />
       </div>
     </Suspense>
   );
