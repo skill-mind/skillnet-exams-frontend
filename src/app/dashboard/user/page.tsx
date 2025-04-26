cc2a05304e2f7e664c3b0c78a83fc48710289fa0
jahrulezfrancis <longsaarmuknaan@gmail.com>
2025-04-26 22:06:23 +0100

    Added component data

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,8 +1,13 @@
+"use client";
 
+import DashboardLayout from '@/components/dashboard/exam/components/new-dashboard-layout';
+import ExamsPageHeaderAndContent from '@/components/dashboard/exam/components/ui/exams-page-content-header';
 
-function Page(){
-    <>
-    <h1>User dashboard</h1>
-    </>
+export default function Dashboard() {
+
+    return (
+        <DashboardLayout title="Dashboard" activePage="dashboard">
+            <ExamsPageHeaderAndContent />
+        </DashboardLayout>
+    );
 }
-export default Page;
