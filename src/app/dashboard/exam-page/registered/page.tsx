638d13a1d37316696630b7a97e240fd2069a02a6
jahrulezfrancis <longsaarmuknaan@gmail.com>
2025-04-26 16:22:45 +0100

    removed deleted component

--- src/app/dashboard/exam-page/registered/page.tsx
+++ src/app/dashboard/exam-page/registered/page.tsx
@@ -1,4 +1,3 @@
-import DynamicHeader from "@/components/dashboard/exam/components/dynamic-header";
 import ExamsRegisteredComponent from "@/components/exams-registered/ExamsRegistered";
 import Image from "next/image";
 import { Suspense } from "react";
@@ -8,7 +7,6 @@ export default function RegisteredPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div className="flex flex-col">
-        <DynamicHeader />
         <div className="self-end pt-14 text-[#ABABAB] flex gap-2">
           <p>Chatbox</p>
           <Image alt="robot-chat-image" width={20} height={20} src={RobotChatImage} />
